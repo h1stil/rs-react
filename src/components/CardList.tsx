0081be296da096c6e1a9bbe439df4c19f804e6be
h1stil <jkaem111@gmail.com>
2023-03-31 21:55:04 +0300

    feat: refactor for hooks HomePage

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,60 +1,47 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MyState } from 'utils/types';
 import Card from './Card';
 
-class CardList extends Component<object, MyState> {
-  constructor(props: MyState) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: [],
-    };
-  }
+const CardList = () => {
+  const [cards, setCards] = useState<MyState>({ error: null, isLoaded: false, items: [] });
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://dummyjson.com/products')
       .then((res) => res.json())
       .then(
         (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.products,
-          });
+          setCards({ isLoaded: true, items: result.products, error });
         },
         (error) => {
-          this.setState({
+          setCards({
             isLoaded: true,
+            items,
             error,
           });
         }
       );
-  }
+  });
 
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    }
-    if (!isLoaded) {
-      return <div style={{ textAlign: 'center' }}>Loading...</div>;
-    }
-    return (
-      <div className="card__list">
-        {Array.isArray(items) ? (
-          items.map((item) => (
+  const { error, isLoaded, items } = cards;
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (!isLoaded) {
+    return <div style={{ textAlign: 'center' }}>Loading...</div>;
+  }
+  return (
+    <div className="card__list">
+      {Array.isArray(items)
+        ? items.map((item) => (
             <div key={item.id}>
               <Card {...item} />
             </div>
           ))
-        ) : (
-          <p>FF</p>
-        )}
-      </div>
-    );
-  }
-}
+        : null}
+    </div>
+  );
+};
 
 export default CardList;
