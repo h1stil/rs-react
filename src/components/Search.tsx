8f28eae9713a976d1d0a0875714f9a608909c6a2
h1stil <jkaem111@gmail.com>
2023-03-31 22:05:24 +0300

    refactor: search input with hooks

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,64 +1,47 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '../styles/_search.scss';
 
-interface MyProps {
-  search?: string;
-}
-interface MyState {
-  search: string;
-}
+const Search = () => {
+  const [searchValue, setSearchValue] = useState('');
 
-class Search extends Component<MyProps, MyState> {
-  constructor(props: MyProps) {
-    super(props);
-    this.state = {
-      search: '',
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const search = localStorage.getItem('search');
     if (search) {
-      this.setState({ search });
+      setSearchValue(search);
     }
-  }
+  }, []);
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const input = event.target;
-    const { value } = input;
-    this.setState({ search: value });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    setSearchValue(input);
   };
 
-  handleFormSubmit = () => {
-    const { search } = this.state;
-    localStorage.setItem('search', search);
+  const handleFormSubmit = () => {
+    localStorage.setItem('search', searchValue);
   };
 
-  render() {
-    const { search } = this.state;
-    return (
-      <div className="search__container">
-        <form>
-          <input
-            type="text"
-            placeholder="Search..."
-            name="search"
-            autoComplete="off"
-            value={search}
-            onChange={this.handleChange}
-            onBlur={this.handleFormSubmit}
-            id="search-input"
-          />
-          <button type="submit">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search__container">
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          name="search"
+          autoComplete="off"
+          value={searchValue}
+          onChange={handleChange}
+          onBlur={() => handleFormSubmit()}
+          id="search-input"
+        />
+        <button type="submit">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Search;
