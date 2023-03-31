9380a25cdde423e52bd3d0a5d2af5203b23a927c
h1stil <jkaem111@gmail.com>
2023-03-31 21:45:25 +0300

    init: start task

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,31 +1,22 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 
 import '../styles/_card.scss';
 import { MyCard } from 'utils/types';
 
-class Card extends Component<MyCard> {
-  constructor(props: MyCard) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { title, images, description, stock, price } = this.props;
-
-    return (
-      <div className="card">
-        <h4 className="card__title">{title}</h4>
-        <img src={images[0]} alt="card" />
-        <div className="card__description">
-          <p className="desciption__text">{description}</p>
-        </div>
-        <div className="card__price">
-          <p className="price__stock">stock: {stock}</p>
-          <p className="price__price">price: ${price}</p>
-        </div>
+const Card: FC<MyCard> = (props) => {
+  return (
+    <div className="card">
+      <h4 className="card__title">{props.title}</h4>
+      <img src={props.thumbnail} alt="card" />
+      <div className="card__description">
+        <p className="desciption__text">{props.description}</p>
       </div>
-    );
-  }
-}
+      <div className="card__price">
+        <p className="price__stock">stock: {props.stock}</p>
+        <p className="price__price">price: ${props.price}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
