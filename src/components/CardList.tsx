437e465f5c944fbce7b74270502221045842141b
h1stil <jkaem111@gmail.com>
2023-04-08 17:32:37 +0300

    feat: added search

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -9,26 +9,55 @@ function CardList() {
   const [cards, setCards] = useState<MyState>({ isError: null, isLoaded: false, items: [] });
   const { isError, isLoaded, items } = cards;
   const [page, setPage] = useState(1);
+  const [searchInput, setSearchInput] = useState(localStorage.getItem('search') || '');
 
   const ITEMS_PER_PAGE = 10;
   const TOTAL_ITEMS = 100;
+
   useEffect(() => {
-    fetch(`https://dummyjson.com/products?limit=10&skip=${(page - 1) * ITEMS_PER_PAGE}`)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setCards({ isLoaded: true, items: result.products, isError });
-        },
-        (error) => {
-          setCards({
-            isLoaded: true,
-            items,
-            isError: error,
-          });
-        }
-      );
-    setCards({ isLoaded: false, isError, items });
-  }, [page]);
+    const handleStorage = () => {
+      setSearchInput(localStorage.getItem('search') || '');
+    };
+
+    window.addEventListener('storage', handleStorage);
+    return () => window.removeEventListener('storage', handleStorage);
+  }, []);
+
+  useEffect(() => {
+    if (searchInput) {
+      fetch(`https://dummyjson.com/products/search?q=${searchInput}`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setCards({ isLoaded: true, items: result.products, isError });
+          },
+          (error) => {
+            setCards({
+              isLoaded: true,
+              items,
+              isError: error,
+            });
+          }
+        );
+      setCards({ isLoaded: false, isError, items });
+    } else {
+      fetch(`https://dummyjson.com/products?limit=10&skip=${(page - 1) * ITEMS_PER_PAGE}`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setCards({ isLoaded: true, items: result.products, isError });
+          },
+          (error) => {
+            setCards({
+              isLoaded: true,
+              items,
+              isError: error,
+            });
+          }
+        );
+      setCards({ isLoaded: false, isError, items });
+    }
+  }, [searchInput, page]);
 
   if (isError) {
     return <div>Error: {isError.message}</div>;
@@ -37,50 +66,55 @@ function CardList() {
     return <div style={{ textAlign: 'center' }}>Loading...</div>;
   }
 
-  console.log(page);
   return (
     <>
       <div className="card__list">
-        {Array.isArray(items)
-          ? items.map((item) => (
+        {items.length ? (
+          Array.isArray(items) ? (
+            items.map((item) => (
               <div key={item.id}>
                 <Card {...item} />
               </div>
             ))
-          : null}
-      </div>
-      <div className="pages">
-        {page > 1 ? (
-          <button
-            className="pages_prev "
-            onClick={() => {
-              setPage(page - 1);
-              setCards({ isLoaded: true, isError, items });
-            }}
-          >
-            &laquo;
-          </button>
+          ) : null
         ) : (
-          <button className="disabled" disabled>
-            &laquo;
-          </button>
-        )}
-        {page < TOTAL_ITEMS / ITEMS_PER_PAGE ? (
-          <button
-            className="pages_next"
-            onClick={() => {
-              setPage(page + 1);
-              setCards({ isLoaded: true, isError, items });
-            }}
-          >
-            &raquo;
-          </button>
-        ) : (
-          <button className="disabled" disabled>
-            &raquo;
-          </button>
+          <p>Not found</p>
         )}
       </div>
+      {items.length >= ITEMS_PER_PAGE ? (
+        <div className="pages">
+          {page > 1 ? (
+            <button
+              className="pages_prev "
+              onClick={() => {
+                setPage(page - 1);
+                setCards({ isLoaded: true, isError, items });
+              }}
+            >
+              &laquo;
+            </button>
+          ) : (
+            <button className="disabled" disabled>
+              &laquo;
+            </button>
+          )}
+          {page < TOTAL_ITEMS / ITEMS_PER_PAGE ? (
+            <button
+              className="pages_next"
+              onClick={() => {
+                setPage(page + 1);
+                setCards({ isLoaded: true, isError, items });
+              }}
+            >
+              &raquo;
+            </button>
+          ) : (
+            <button className="disabled" disabled>
+              &raquo;
+            </button>
+          )}
+        </div>
+      ) : null}
     </>
   );
 }
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -14,6 +14,12 @@ function Search() {
     }
   }, []);
 
+  // useEffect(() => {
+  //   window.addEventListener('storage', () => {
+  //     console.log('change');
+  //   });
+  // }, []);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const input = event.target.value;
     setSearchValue(input);
@@ -21,11 +27,17 @@ function Search() {
 
   const handleFormSubmit = () => {
     localStorage.setItem('search', searchValue);
+    window.dispatchEvent(new Event('storage'));
   };
 
   return (
     <div className="search__container">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleFormSubmit();
+        }}
+      >
         <input
           type="text"
           placeholder="Search..."
